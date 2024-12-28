61b3c48a43c2b0718aa79222e1afac722a2884c2
Cristian Perez <cristian.perez.domper@gmail.com>
2024-12-28 18:31:49 -0500

    Tienda Api

---
+++ Infrastructure/Data/Configuration/MarcaConfiguration.cs
@@ -0,0 +1,20 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configuration;
+
+public class MarcaConfiguration : IEntityTypeConfiguration<Marca>
+{
+    public void Configure(EntityTypeBuilder<Marca> builder)
+    {
+        builder.ToTable("Marca");
+
+        builder.Property(m => m.Id)
+               .IsRequired();
+
+        builder.Property(m => m.Nombre)
+               .IsRequired()
+               .HasMaxLength(100);
+    }
+}
